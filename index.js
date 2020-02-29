3a9789654a40537a3aa4194e483b6305c587a113
Stefan Stölzle <stoe@github.com>
2020-02-29 15:58:23 +0100

    Init

---
+++ index.js
@@ -0,0 +1,137 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const yaml = require('js-yaml')
+const moment = require('moment')
+
+async function run() {
+  try {
+    let response
+
+    const token = core.getInput('repo-token', {required: true})
+    const client = new github.GitHub(token)
+
+    const {context} = github
+    const {owner, repo} = context.repo
+
+    // get the configuration
+    let path
+
+    try {
+      path = core.getInput('configuration-path', {required: true})
+    } catch (err) {
+      throw new Error(`config file not found`)
+    }
+
+    response = await client.repos.getContents({
+      owner,
+      repo,
+      path,
+      ref: context.ref
+    })
+
+    const content = Buffer.from(response.data.content, 'base64').toString()
+    const {manager, report, label, title, template} = yaml.safeLoad(content)
+
+    // check if manager and report have write access to the repo
+    const {repository} = await client.graphql(
+      `query($owner: String!, $repo: String!, $manager: String!, $report: String!) {
+        repository(owner: $owner, name: $repo) {
+          manager: assignableUsers(query: $manager) {
+            totalCount
+          }
+          report: assignableUsers(query: $report) {
+            totalCount
+          }
+        }
+      }`,
+      {
+        owner,
+        repo,
+        manager,
+        report
+      }
+    )
+
+    if (repository.manager.totalCount === 0) {
+      throw new Error(`@${manager} has no write access to ${owner}/${repo}`)
+    }
+
+    if (repository.report.totalCount === 0) {
+      throw new Error(`@${report} has no write access to ${owner}/${repo}`)
+    }
+
+    // check if 1:1 label exists and created it if not
+    try {
+      await client.issues.getLabel({
+        owner,
+        repo,
+        name: label,
+        description: '1:1 meeting agenda and notes'
+      })
+    } catch (err) {
+      await client.issues.createLabel({
+        owner,
+        repo,
+        name: label,
+        description: '1:1 meeting agenda and notes',
+        color: '4078c0'
+      })
+    }
+
+    // check for old open 1:1 issues and close them
+    const {data} = await client.issues.listForRepo({
+      owner,
+      repo,
+      labels: label,
+      state: 'open',
+      sort: 'created',
+      direction: 'asc'
+    })
+
+    const refs = []
+
+    for (const issue of data) {
+      refs.push(issue.number)
+
+      try {
+        await client.graphql(
+          `mutation ($id: ID!) {
+            closeIssue(input: {
+              issueId: $id
+            }) {
+              issue {
+                number
+              }
+            }
+          }`,
+          {
+            id: issue.node_id
+          }
+        )
+      } catch (err) {
+        continue
+      }
+    }
+
+    const body = template
+      .replace('{% last %}', refs.length === 0 ? 'n/a' : `#${refs.join(' #')}`)
+      .replace('{% manager %}', manager)
+      .replace('{% report %}', report)
+
+    // open new 1:1 issue, label it and assign it to manager and report
+    response = await client.issues.create({
+      owner,
+      repo,
+      title: title || `@${manager}/@${report} 1:1 Topics ${moment().format('M/D/YYYY')}`,
+      body,
+      labels: [label],
+      assignees: [manager, report]
+    })
+
+    core.setOutput('url', response.data.html_url)
+  } catch (err) {
+    core.setFailed(err.message)
+  }
+}
+
+run()
---
+++ readme.md
@@ -0,0 +1,99 @@
+# meeting-1on1-action
+
+> This GitHub Action creates a new issue for a 1:1 meeting with your manager.
+
+![Test](https://github.com/stoe/meeting-1on1-action/workflows/Test/badge.svg) [![styled with prettier](https://img.shields.io/badge/styled_with-prettier-ff69b4.svg)](https://github.com/prettier/prettier)
+
+## Usage
+
+```yml
+# .github/workflows/schedule.yml
+on:
+  schedule:
+    # every Monday at 01:00 UTC
+    - cron: '0 1 * * 1'
+
+jobs:
+  schedule:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v2
+      - uses: stoe/meeting-1on1-action@master
+        with:
+          repo-token: ${{ secrets.GITHUB_TOKEN }}
+```
+
+This reads from a `.github/101.yml` configuration file within your repository.
+
+### Configuration file
+
+To use the `meeting-1on1-action` you have to provide a configuration file within your repository.
+
+#### Example
+
+```yml
+# (required) manager's github.com username witout the `@`
+manager: my_manager
+
+# (required) report's github.com username witout the `@`
+report: stoe
+
+# (required) label is created if not already available
+label: '1:1 meeting'
+
+# (optional) title
+#   if not provided will default to
+#   e.g. @my_manager/@stoe 1:1 Topics 2/29/2020
+#   with current date automatically added
+# set this to `title: false` to use the default
+title: '@{% manager %}/@{% report %} 1:1 Topics'
+
+# (required)
+template: |
+  ### Last 1:1
+
+  {% last %}
+
+  ### Topics for today's meeting
+
+  - [ ] ...
+```
+
+#### Variables
+
+The above `template` field has a `{% last %}` variable.
+
+The available variables are:
+
+- `{% last %}`: auto filled with reference link(s) to the last 1:1 meeting issue(s)
+
+- `{% manager %}`: auto filled with the manager's github.com username provided in the config file
+
+- `{% report %}`: auto filled with the reports's github.com username provided in the config file
+
+
+### Inputs
+
+- `repo-token`: `${{ secrets.GITHUB_TOKEN }}` Token to authenticate within the workflow run. More info available [here](https://help.github.com/en/actions/configuring-and-managing-workflows/authenticating-with-the-github_token#about-the-github_token-secret).
+
+- `configuration-path`: (optional) Set the path to your configuration file within the repository. Defaults to `.github/101.yml`.
+
+### Outputs
+
+If you need the Issue URL of the created 1:1 meeting issue for another step, you can use the `url` output.
+
+For example:
+
+```yml
+steps:
+  - uses: stoe/meeting-1on1-action@master
+    id: issue
+    with:
+      repo-token: ${{ secrets.GITHUB_TOKEN }}
+  - name: '1:1 issue URL'
+    run: echo "${{ steps.issue.outputs.url }}"
+```
+
+## License
+
+[MIT](./license) © [Stefan Stölzle](https://github.com/stoe)
